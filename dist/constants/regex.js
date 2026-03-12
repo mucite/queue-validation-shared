408a56b2ba3790eae1321a8da16ce835fc49ba98
mucite <musie_kebede@icloud.com>
2026-03-12 09:35:03 +0100

    Update password validation to require minimum 4 characters only (remove complexity requirements)

--- dist/constants/regex.js
+++ dist/constants/regex.js
@@ -3,5 +3,5 @@
  */
 // Ethiopian phone number (09xxxxxxxx or 07xxxxxxxx)
 export const ETHIOPIAN_PHONE_REGEX = /^0[79]\d{8}$/;
-// Password: minimum 8 characters, at least one uppercase, one lowercase, one digit
-export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+// Password: minimum 4 characters only (no complexity requirements)
+export const PASSWORD_REGEX = /^.{4,}$/;
